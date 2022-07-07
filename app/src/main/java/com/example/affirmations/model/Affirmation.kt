15c288c1aa494d5d05a385f34e2ceef488f30563
yanisapths <72002605+yanisapths@users.noreply.github.com>
2022-07-07 07:48:10 +0700

    display images

--- app/src/main/java/com/example/affirmations/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations/model/Affirmation.kt
@@ -1,8 +1,16 @@
 package com.example.affirmations.model
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
 //com.example.affirmations.model is a package for class that model(or represent) the data, will contain any data-related in this app.
 //a class called "Affirmation" contains the "resourceID" of the string defined in this app.
 //make Affirmation a data class by adding the "data" keyword before the class definition, and must have at least one property defined .
-data class Affirmation(val stringResourceId: Int) {
-
-}
+data class Affirmation(
+    /*
+        1.Add the @String res to stringResourceId
+        2.Add @Drawable to imageResourceId
+    */
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
